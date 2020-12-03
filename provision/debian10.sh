e8396a061c0f7b25addb37c326bd88fdbe876195
FelipeVim <felipe.souza.valim@gmail.com>
2020-12-03 14:57:11 -0300

    Removido cópia do arquivo locale.

--- provision/debian10.sh
+++ provision/debian10.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-BASEDIR='files/centos8'
-
-#sudo cp "${BASEDIR}/locale" /etc/default/locale
-
 sudo apt update
 
 sudo apt install vim net-tools  mailutils tzdata  sysstat  traceroute iotop locales-all parted lvm2 curl docker-compose docker.io -y
