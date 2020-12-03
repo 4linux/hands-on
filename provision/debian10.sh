756358b883458e865068dcba2342119486b7ed35
Hector Vido <hector_vido@yahoo.com.br>
2020-12-03 15:01:14 -0300

    Utilizado dnf e apt-get. Agrupado comandos similares

--- provision/debian10.sh
+++ provision/debian10.sh
@@ -4,10 +4,8 @@ BASEDIR='files/centos8'
 
 #sudo cp "${BASEDIR}/locale" /etc/default/locale
 
-sudo apt update
+apt-get update
 
-sudo apt install vim net-tools  mailutils tzdata  sysstat  traceroute iotop locales-all parted lvm2 curl dialog docker-compose -y
+apt-get install -y vim net-tools mailutils tzdata sysstat traceroute iotop locales-all parted lvm2 curl dialog docker-compose docker.io
 
 sudo timedatectl set-timezone America/Sao_Paulo
-
-curl -sSL https://get.docker.com/ | sh
