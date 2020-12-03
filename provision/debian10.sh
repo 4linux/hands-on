e04ad20b01a6d68e0a249155ef1d1337f4caf42e
FelipeVim <felipe.souza.valim@gmail.com>
2020-12-03 15:17:40 -0300

    Removido sudo dos comandos.

--- provision/debian10.sh
+++ provision/debian10.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt update
+apt update
 
-apt-get install -y vim net-tools mailutils tzdata sysstat traceroute iotop locales-all parted lvm2 curl dialog docker-compose docker.io
+apt install vim net-tools  mailutils tzdata  sysstat  traceroute iotop locales-all parted lvm2 curl dialog docker-compose docker.io -y
 
-sudo timedatectl set-timezone America/Sao_Paulo
+timedatectl set-timezone America/Sao_Paulo
