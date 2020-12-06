b8d88687b4fd7bcc708d448770c7f211ea189a1a
FelipeVim <felipe.souza.valim@gmail.com>
2020-12-06 12:46:07 -0300

    Efetuado alteração de apt para apt-get no script de provision.

--- provision/debian10.sh
+++ provision/debian10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update
+apt-get update
 
 apt-get install -y vim net-tools mailutils tzdata sysstat traceroute iotop locales-all parted lvm2 curl dialog docker-compose docker.io
 
