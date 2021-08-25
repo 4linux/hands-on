c7b95290df7c5b9a51f861f7f5bcb417b8dd4b23
Felipe Valim <felipe.souza.valim@gmail.com>
2021-08-25 10:21:15 -0300

    Scripts de provisionamento atualizados

--- provision/centos8.sh
+++ provision/centos8.sh
@@ -7,15 +7,9 @@ setenforce 0
 dnf install -y chrony bind-utils net-tools tcpdump telnet vim epel-release  wget  mailx tzdata tzdata-java  sysstat  traceroute iotop langpacks-en glibc-all-langpacks bc openldap-clients lvm2 network-scripts
 
 systemctl disable firewalld NetworkManager
-systemctl enable network
 
-> /etc/sysconfig/disable-deprecation-warnings
+systemctl enable network && > /etc/sysconfig/disable-deprecation-warnings
 
 systemctl stop firewalld NetworkManager
 
 timedatectl set-timezone America/Sao_Paulo
-
-id devops
-if [ "$?" -ne 0 ]; then
-    useradd -m -d /home/devops -s /bin/bash devops
-fi
--- provision/debian10.sh
+++ provision/debian10.sh
@@ -2,11 +2,6 @@
 
 apt-get update
 
-apt-get install -y vim net-tools mailutils tzdata sysstat traceroute iotop locales-all parted lvm2 curl dialog docker-compose docker.io
+apt-get install -y vim net-tools mailutils tzdata sysstat traceroute iotop locales-all parted lvm2 curl dialog
 
 timedatectl set-timezone America/Sao_Paulo
-
-id devops
-if [ "$?" -ne 0 ]; then
-    useradd -m -d /home/devops -s /bin/bash devops
-fi
