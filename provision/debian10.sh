892db682d4b45b33d79820c4e31efc45457a71a1
Felipe Valim <felipe.souza.valim@gmail.com>
2021-08-25 15:52:03 -0300

    adicionado pacote gnupg no debian

--- provision/debian10.sh
+++ provision/debian10.sh
@@ -2,6 +2,6 @@
 
 apt-get update
 
-apt-get install -y vim net-tools mailutils tzdata sysstat traceroute iotop locales-all parted lvm2 curl dialog
+apt-get install -y vim net-tools mailutils tzdata sysstat traceroute iotop locales-all parted lvm2 curl dialog gnupg
 
 timedatectl set-timezone America/Sao_Paulo
